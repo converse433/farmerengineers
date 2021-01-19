7980f4264a023e5b88bc0959f25ad58584ac056e
converse433 <77678871+converse433@users.noreply.github.com>
2021-01-19 23:29:28 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -23,21 +23,3 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
-
-var swiper = new Swiper('.swiper-container', {
-	effect: 'coverflow',
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: 'auto',
-	coverflowEffect: {
-		rotate: 0,
-		stretch: 0,
-		depth: 200,
-		modifier: 1,
-		slideShadows: true,
-	},
-	pagination: {
-		el: '.swiper-pagination',
-	},
-	loop: true,
-});
